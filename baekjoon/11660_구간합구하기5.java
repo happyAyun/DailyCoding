b52aff83570916d806404d15a579b57b048d0e28
SIM AYUN <76723441+happyAyun@users.noreply.github.com>
2022-03-01 14:14:02 +0900

    Create 11660_구간합구하기5.java

---
+++ baekjoon/11660_구간합구하기5.java
@@ -0,0 +1,32 @@
+package algo;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_11660_구간합구하기5 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			for (int j = 1; j <= N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken()) + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1];
+			}
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			sb.append(arr[x][y] - arr[a - 1][y] - arr[x][b - 1] + arr[a - 1][b - 1] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
